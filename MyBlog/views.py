4bed94a6d2ad9eceaf8784d0facd88c4d403baf3
Jan Gazda <1oglop1@gmail.com>
2016-04-25 21:27:19 +0200

    Modified urls.py views.py templates/posts_list.html
    to display posts from database

--- MyBlog/views.py
+++ MyBlog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+
+    posts = Post.objects.filter(date_published__lte=timezone.now()).order_by('-date_published')[:6]
+
+    return render(request, 'MyBlog/post_list.html', {'posts': posts})
