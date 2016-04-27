dbfe0a87bdabb9250e75c458d60233d92a7b7241
Jan Gazda <1oglop1@gmail.com>
2016-04-27 20:57:20 +0200

    finished extend your application, forms to go

--- MyBlog/views.py
+++ MyBlog/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 # Create your views here.
+
+
 def post_list(request):
 
     posts = Post.objects.filter(date_published__lte=timezone.now()).order_by('-date_published')[:6]
 
     return render(request, 'MyBlog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    return render(request, 'MyBlog/post_detail.html', {'post': post})
